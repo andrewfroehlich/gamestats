4b983294079ef91f0692a68dc13830d68f859b04
Andrew F <andrew.r.froehlich@gmail.com>
2021-01-18 19:40:09 +0000

    Tabs to spaces

--- shoots_and_ladders.py
+++ shoots_and_ladders.py
@@ -4,22 +4,22 @@ import math
 totalruns = 5000
 
 pairs = {
-	1:38,
-	4:14,
-	9:31,
-	21:42,
-	28:84,
-	51:67,
-	71:91,
-	80:100,
-	17:7,
-	54:34,
-	62:19,
-	64:60,
-	87:24,
-	93:73,
-	95:75,
-	98:79
+    1:38,
+    4:14,
+    9:31,
+    21:42,
+    28:84,
+    51:67,
+    71:91,
+    80:100,
+    17:7,
+    54:34,
+    62:19,
+    64:60,
+    87:24,
+    93:73,
+    95:75,
+    98:79
 }
 
 totalturns = 0 #sum of all turns, for average
@@ -28,28 +28,28 @@ turnfrequency = [0]*300 #count of occurrences per turn number for graphing
 mode_turns = 0
 mode_count = 0
 for run in range(totalruns):
-	turns = 0
-	position = 0
-	while position != 100:
-		turns += 1
-		roll = random.randint(1,6)
-		newposition = position + roll
-		if newposition in pairs:
-			position = pairs[newposition]
-		elif newposition <= 100:
-			position = newposition
-	turnlist.append(turns)
-	totalturns += turns
-	turnfrequency[turns] = turnfrequency[turns] + 1
-	if turnfrequency[turns] > mode_count:
-		mode_turns = turns
-		mode_count = turnfrequency[turns]
+    turns = 0
+    position = 0
+    while position != 100:
+        turns += 1
+        roll = random.randint(1,6)
+        newposition = position + roll
+        if newposition in pairs:
+            position = pairs[newposition]
+        elif newposition <= 100:
+            position = newposition
+    turnlist.append(turns)
+    totalturns += turns
+    turnfrequency[turns] = turnfrequency[turns] + 1
+    if turnfrequency[turns] > mode_count:
+        mode_turns = turns
+        mode_count = turnfrequency[turns]
 
 turnlist.sort()
 
 def turnlist_percentile(perc):
-	return turnlist[int(round(len(turnlist)*perc/100.0))]
-	
+    return turnlist[int(round(len(turnlist)*perc/100.0))]
+    
 print("Over " + str(totalruns) + " runs:")
 print("Minimum: " + str(turnlist[0]))
 print("5th Percentile: " + str(turnlist_percentile(5)))
@@ -64,4 +64,4 @@ print("Maximum: " + str(turnlist[totalruns-1]))
 print("=========")
 print("Occurrence Graph:")
 for turnnumber in range(turnlist[0],turnlist[totalruns-1]+1):
-	print(str(turnnumber) + ":" + ("x"*turnfrequency[turnnumber]))
+    print(str(turnnumber) + ":" + ("x"*turnfrequency[turnnumber]))
